5c47ef384537d49deaede3b09354a0223bf68708
Haruto Taketomi <takebamboo88@gmail.com>
2025-06-24 10:04:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -79,13 +79,8 @@ void main()
    
    
    initialize();
-   
    int1 is_finished = FALSE;
-   read_id(smf);
    
-   int8 a[8] = {0}; 
-   example_00(a);
-   copy_data();
 
    //Start loop
    while(!is_finished)
